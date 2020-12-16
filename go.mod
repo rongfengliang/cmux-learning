129bce842c9788e848128e56bef860c89020ca66
rong <1141591465@qq.com>
2020-12-16 13:42:10 +0800

    init project

---
+++ go.mod
@@ -0,0 +1,14 @@
+module demoapp
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/soheilhy/cmux v0.1.4
+	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11
+	golang.org/x/sys v0.0.0-20201214210602-f9fddec55a1e // indirect
+	golang.org/x/text v0.3.4 // indirect
+	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d // indirect
+	google.golang.org/grpc v1.34.0
+	google.golang.org/grpc/examples v0.0.0-20201215235606-668e3ae6d589
+)
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"strings"
+
+	"github.com/soheilhy/cmux"
+	"golang.org/x/net/websocket"
+	"google.golang.org/grpc"
+	grpchello "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+type exampleHTTPHandler struct{}
+
+func (h *exampleHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "example http response")
+}
+
+func serveHTTP(l net.Listener) {
+	s := &http.Server{
+		Handler: &exampleHTTPHandler{},
+	}
+	if err := s.Serve(l); err != cmux.ErrListenerClosed {
+		panic(err)
+	}
+}
+
+func EchoServer(ws *websocket.Conn) {
+	if _, err := io.Copy(ws, ws); err != nil {
+		panic(err)
+	}
+}
+
+func serveWS(l net.Listener) {
+	s := &http.Server{
+		Handler: websocket.Handler(EchoServer),
+	}
+	if err := s.Serve(l); err != cmux.ErrListenerClosed {
+		panic(err)
+	}
+}
+
+type ExampleRPCRcvr struct{}
+
+func (r *ExampleRPCRcvr) Cube(i int, j *int) error {
+	*j = i * i
+	return nil
+}
+
+func serveRPC(l net.Listener) {
+	s := rpc.NewServer()
+	if err := s.Register(&ExampleRPCRcvr{}); err != nil {
+		panic(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			if err != cmux.ErrListenerClosed {
+				panic(err)
+			}
+			return
+		}
+		go s.ServeConn(conn)
+	}
+}
+
+type grpcServer struct {
+	// must embedd this
+	grpchello.UnimplementedGreeterServer
+}
+
+func (s *grpcServer) SayHello(ctx context.Context, in *grpchello.HelloRequest) (
+	*grpchello.HelloReply, error) {
+
+	return &grpchello.HelloReply{Message: "Hello " + in.Name + " from cmux"}, nil
+}
+func serveGRPC(l net.Listener) {
+	grpcs := grpc.NewServer()
+	grpchello.RegisterGreeterServer(grpcs, &grpcServer{})
+	if err := grpcs.Serve(l); err != cmux.ErrListenerClosed {
+		panic(err)
+	}
+}
+
+type mylogin struct {
+}
+
+func (my mylogin) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.Header().Add("Content-Type", "text/html")
+	res.Write([]byte("dalong demo"))
+}
+func main() {
+	l, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	m := cmux.New(l)
+
+	// We first match the connection against HTTP2 fields. If matched, the
+	// connection will be sent through the "grpcl" listener.
+	grpcl := m.Match(cmux.HTTP2HeaderFieldPrefix("content-type", "application/grpc"))
+	//Otherwise, we match it againts a websocket upgrade request.
+	wsl := m.Match(cmux.HTTP1HeaderField("Upgrade", "websocket"))
+
+	// Otherwise, we match it againts HTTP1 methods. If matched,
+	// it is sent through the "httpl" listener.
+	httpl := m.Match(cmux.HTTP1Fast())
+	// If not matched by HTTP, we assume it is an RPC connection.
+	rpcl := m.Match(cmux.Any())
+
+	// Then we used the muxed listeners.
+	go serveGRPC(grpcl)
+	go serveWS(wsl)
+	go serveHTTP(httpl)
+	go serveRPC(rpcl)
+
+	if err := m.Serve(); !strings.Contains(err.Error(), "use of closed network connection") {
+		panic(err)
+	}
+}
